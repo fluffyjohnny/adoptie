b623345029b57d29553a780fce2d73442138d336
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-09-24 19:51:49 -0700

    make index login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable>
+        <Link href={"/login"}>
+          <Text>Go to Login Screen</Text>
+        </Link>
+      </Pressable>
     </View>
   );
 }
---
+++ app/login/index.tsx
@@ -0,0 +1,69 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+
+export default function LoginScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        source={require("./../../assets/images/login.png")}
+        style={styles.image}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Ready to make a new friend?</Text>
+        <Text style={styles.description}>
+          Let's find your next best friend.
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.btnText}>Let's go</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    height: "100%",
+  },
+  image: {
+    width: "100%",
+    height: 500,
+  },
+  textContainer: {
+    padding: 20,
+    display: "flex",
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "outfit-bold",
+    fontSize: 30,
+    textAlign: "center",
+  },
+  description: {
+    fontFamily: "outfit",
+    fontSize: 18,
+    textAlign: "center",
+    color: Colors.GRAY,
+  },
+  button: {
+    padding: 14,
+    marginTop: 50,
+    backgroundColor: Colors.PRIMARY,
+    width: "100%",
+    borderRadius: 14,
+  },
+  btnText: {
+    fontFamily: "outfit-medium",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
