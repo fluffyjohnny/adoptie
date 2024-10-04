b6193fc0ddd47de6f7566a36c245f54cb4948a20
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-10-03 23:30:49 -0700

    add clerk to project + setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function RootLayout() {
   useFonts({
@@ -9,8 +18,12 @@ export default function RootLayout() {
   });
 
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
