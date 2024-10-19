061b36266374d41af13a6e0f54aaf7060fbc4a1a
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-10-18 23:48:20 -0700

    add slider functionalities

--- app/components/slider.tsx
+++ app/components/slider.tsx
@@ -2,8 +2,11 @@ import { View, Text } from "react-native";
 import React, { useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/config/FirebaseConfig";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function Slider() {
+  const [sliders, setSliders] = React.useState<any[]>([]);
+
   useEffect(() => {
     getSliders();
   }, []);
@@ -12,12 +15,20 @@ export default function Slider() {
     const docRef = await getDocs(collection(db, "sliders"));
     docRef.forEach((doc) => {
       console.log(doc.data());
+      setSliders((sliders) => [...sliders, doc.data()]);
     });
   };
 
   return (
     <View>
-      <Text>Slider</Text>
+      <FlatList
+        data={sliders}
+        renderItem={({ item, index }) => (
+          <View>
+            <Image source={{ uri: item?.url }} />
+          </View>
+        )}
+      />
     </View>
   );
 }
