994544e14eb769c6cf3952fae3a54479bb304162
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-10-21 16:08:00 -0700

    relocate components to the correct folder structure

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
-import Header from "../components/header";
-import Slider from "../components/slider";
+import Header from "../../components/header";
+import Slider from "../../components/slider";
 
 export default function Home() {
   return (
