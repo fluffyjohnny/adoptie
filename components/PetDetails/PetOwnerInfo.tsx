83e539cc7c74c815e632bc1b09ef3bb6591c11ec
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-11-16 19:40:30 -0800

    add send message icon

--- components/PetDetails/PetOwnerInfo.tsx
+++ components/PetDetails/PetOwnerInfo.tsx
@@ -1,14 +1,22 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function PetOwnerInfo({ pet }: any) {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: pet?.ownerUrl }} style={styles.image} />
-      <View>
-        <Text style={styles.title}>{pet?.ownerName}</Text>
-        <Text style={styles.subtitle}>Pet Owner</Text>
+      <View style={styles.innerContainer}>
+        <View style={styles.ownerInfo}>
+          <Image source={{ uri: pet?.ownerUrl }} style={styles.image} />
+          <View>
+            <Text style={styles.title}>{pet?.ownerName}</Text>
+            <Text style={styles.subtitle}>Pet Owner</Text>
+          </View>
+        </View>
+        <TouchableOpacity>
+          <Ionicons name="send-sharp" size={20} />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -17,14 +25,23 @@ export default function PetOwnerInfo({ pet }: any) {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    margin: 15,
+    margin: 20,
+    borderWidth: 1,
+    borderRadius: 15,
+    backgroundColor: Colors.WHITE,
+  },
+  innerContainer: {
+    paddingHorizontal: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  ownerInfo: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
     gap: 20,
-    borderWidth: 1,
-    borderRadius: 15,
-    backgroundColor: Colors.WHITE,
   },
   title: {
     fontFamily: "outfit-bold",
