4c47bef5e1e87cb797fcf77030d99ca717e10f68
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-11-22 21:21:04 -0800

    add favorite page

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import React from "react";
 
 export default function Favorite() {
   return (
-    <View>
-      <Text>Favorite</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Favorite</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 21,
+    fontWeight: "bold",
+    fontFamily: "outfit-bold",
+  },
+});
