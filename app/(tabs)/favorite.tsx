4990f8226c57610d58142839e4eaf6789b668448
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-11-24 23:44:40 -0800

    wip

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,22 +1,48 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-expo";
 import { getFavoriteList } from "@/shared/Shared";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/config/FirebaseConfig";
+import PetListItem from "@/components/Home/PetListItem";
 
 export default function Favorite() {
   const { user } = useUser();
+  const [favoriteID, setFavoriteID] = useState<string[] | null>([]);
+  const [favoritePetList, setFavoritePetList] = useState<any[]>([]);
 
   useEffect(() => {
     user && getFavoriteID();
   }, [user]);
 
   const getFavoriteID = async () => {
-    const res = getFavoriteList(user);
+    const res = await getFavoriteList(user);
+    setFavoriteID(res?.favorites);
+    getFavoritePetList();
+  };
+
+  const getFavoritePetList = async () => {
+    const q = query(collection(db, "Pets"), where("id", "in", favoriteID));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      setFavoritePetList((prev) => [...prev, doc.data()]);
+    });
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Favorite</Text>
+
+      <FlatList
+        data={favoritePetList}
+        numColumns={2}
+        renderItem={({ item, index }) => (
+          <View>
+            <PetListItem pet={item} />
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
