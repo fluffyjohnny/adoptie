8e95f278a8d64c23d138bc5f7938b6b3cd76c138
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2024-11-26 20:49:58 -0800

    add picker for gender and category

--- app/add-new-pet/index.tsx
+++ app/add-new-pet/index.tsx
@@ -4,26 +4,47 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  View,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "expo-router";
 import Colors from "@/constants/Colors";
 import InputField from "@/components/add-pet/InputField";
+import { Picker } from "@react-native-picker/picker";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/config/FirebaseConfig";
 
 export default function AddNewPet() {
   const navigation = useNavigation();
 
   const [formData, setFormData] = useState({});
-
-  const handleChange = (name: string, value: string) => {
-    setFormData({ ...formData, [name]: value });
-  };
+  const [gender, setGender] = useState<string>();
+  const [categories, setCategories] = useState<any[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>();
 
   useEffect(() => {
     navigation.setOptions({
       headerTitle: "",
     });
+
+    getCategories();
   }, []);
+
+  const getCategories = async () => {
+    setCategories([]);
+    const snapshot = await getDocs(collection(db, "Categories"));
+    snapshot.forEach((doc) => {
+      setCategories((x) => [...x, doc.data()]);
+    });
+  };
+
+  const handleChange = (name: string, value: string) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = () => {
+    console.log(formData);
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -38,6 +59,24 @@ export default function AddNewPet() {
         numberOfLines={undefined}
         handleChange={handleChange}
       />
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Category *</Text>
+        <Picker
+          selectedValue={selectedCategory}
+          style={styles.input}
+          onValueChange={(value, index) => {
+            setSelectedCategory(value), handleChange("category", value);
+          }}
+        >
+          {categories.map((category) => (
+            <Picker.Item
+              key={category.name}
+              label={category.name}
+              value={category.name}
+            />
+          ))}
+        </Picker>
+      </View>
       <InputField
         title="Breed*"
         field="breed"
@@ -56,6 +95,20 @@ export default function AddNewPet() {
         numberOfLines={undefined}
         handleChange={handleChange}
       />
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Gender *</Text>
+        <Picker
+          selectedValue={gender}
+          style={styles.input}
+          onValueChange={(value, index) => {
+            setGender(value), handleChange("sex", value);
+          }}
+        >
+          <Picker.Item label={"Male"} value={"male"} />
+          <Picker.Item label={"Female"} value={"female"} />
+          <Picker.Item label={"Unknown"} value={"unknown"} />
+        </Picker>
+      </View>
       <InputField
         title="Address*"
         field="address"
@@ -68,7 +121,7 @@ export default function AddNewPet() {
         numberOfLines={5}
         handleChange={handleChange}
       />
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={onSubmit}>
         <Text style={styles.btnText}>Submit</Text>
       </TouchableOpacity>
     </ScrollView>
@@ -101,5 +154,18 @@ const styles = StyleSheet.create({
   btnText: {
     fontFamily: "outfit-medium",
     textAlign: "center",
+  },
+  inputContainer: {
+    marginVertical: 10,
+  },
+  label: {
+    marginVertical: 5,
+    fontFamily: "outfit",
+  },
+  input: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: Colors.WHITE,
+    fontFamily: "outfit",
   },
 });
